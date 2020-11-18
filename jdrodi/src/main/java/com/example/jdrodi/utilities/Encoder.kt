ad1228ebeaa29db160d0b6d7cb415fc0cc9bc202
Jignesh N Patel <jignesh8992@gmail.com>
2020-11-18 11:50:56 +0530

    library updated

---
+++ jdrodi/src/main/java/com/example/jdrodi/utilities/Encoder.kt
@@ -0,0 +1,17 @@
+package com.example.jdrodi.utilities
+
+import android.util.Base64
+
+/**
+ * DisplayHelper.kt - A simple class to encode and decode string using Base64.
+ * @author  Jignesh N Patel
+ * @date 11-11-2020
+ */
+
+fun String.decode(): String {
+    return Base64.decode(this, Base64.DEFAULT).toString(charset("UTF-8"))
+}
+
+fun String.encode(): String {
+    return Base64.encodeToString(this.toByteArray(charset("UTF-8")), Base64.DEFAULT)
+}
